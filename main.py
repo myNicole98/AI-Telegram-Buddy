f29d7989c706d0b3ce7d7c58df1ae347ee7d3761
nikko <frega.nicole@gmail.com>
2023-05-27 09:49:06 +0200

    Added support to parallel processing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ctransformers import AutoModelForCausalLM
 from telegram.error import BadRequest
 from telegram.ext import *
+import multiprocessing
 import configparser
 import threading
 import time
@@ -13,11 +14,11 @@ config.read('config.ini')
 updater = Updater(token=config.get("Bot", "token"), use_context=True)
 dispatcher = updater.dispatcher
 
-
 # AI model loading
 if config.get("Ai", "uncensored") == "yes":
     censor = "-UNCENSORED"
-else: censor = ""
+else:
+    censor = ""
 
 model = config.get("Ai", "model")
 model_type = config.get("Ai", "model_type")
@@ -26,63 +27,72 @@ path = "models/" + model + "-" + model_size + censor + ".bin"
 
 llm = AutoModelForCausalLM.from_pretrained(path, model_type=model_type, lib=config.get("Ai", "instruction_set"))
 
+# Limit the number of parallel processes
+max_parallel_processes = int(config.get("Chat", "max_parallel_chats"))
+process_semaphore = multiprocessing.Semaphore(max_parallel_processes)
+
+def ai(update, context):
+    process = multiprocessing.Process(target=inference, args=(update, context))
+    process.start()
 
 # AI command
-def ai(update, context):
-    # Get message from user
-    mex = update.message.text
-    mex = mex.replace("/ai", "")
-    response = ""
+def inference(update, context):
+    with process_semaphore:
+        # Get message from user
+        mex = update.message.text
+        mex = mex.replace("/ai", "")
+        response = ""
 
-    # Define the tokens
-    tokens = llm.tokenize(str(config.get("Ai", "prompt") + '\n\n### Instruction:\n\n' + mex + '\n\n ###Response:\n\n'))
-    
-    # Initialize variables
-    sent_message = False
-    prompt_is_done = False
+        # Define the tokens
+        tokens = llm.tokenize(str(config.get("Ai", "prompt") + '\n\n### Instruction:\n\n' + mex + '\n\n ###Response:\n\n'))
 
-    # Define a fragmenter for sending edited messages every second
-    def fragmenter():
-        while not prompt_is_done:
-            try:
-                context.bot.editMessageText(chat_id=update.message.chat_id,
-                                            message_id=context.user_data['bot_last_message_id'],
-                                            text=response)
-            except BadRequest: pass
-            except KeyError: pass
-            time.sleep(int(config.get("Chat", "edit_delay")))
+        # Initialize variables
+        sent_message = False
+        prompt_is_done = False
 
-    # Start the defragmenter and ignore the initial KeyError exception
-    thread = threading.Thread(target=fragmenter)
-    thread.start()
-
-    # Tokenize the response into fragmented responses
-    for token in llm.generate(tokens):
-        fragresp = llm.detokenize(token)
-        response = response + fragresp
-
-        # Send initial message
-        if not sent_message:
-            msg = context.bot.send_message(chat_id=update.effective_chat.id, text=response,
-                  reply_to_message_id=update.message.message_id)
-            context.user_data['bot_last_message_id'] = msg.message_id
-            sent_message = True
-            time.sleep(int(config.get("Chat", "edit_delay")))
-    prompt_is_done = True
-
-    # Edit the message one last time with the final response
-    try:
-        context.bot.editMessageText(chat_id=update.message.chat_id,
+        # Define a fragmenter for sending edited messages every n seconds
+        def fragmenter():
+            while not prompt_is_done:
+                try:
+                    context.bot.editMessageText(chat_id=update.message.chat_id,
                                                 message_id=context.user_data['bot_last_message_id'],
                                                 text=response)
-    except BadRequest: pass
-    # End the threading
-    thread.join()
+                except BadRequest:
+                    pass
+                except KeyError:
+                    pass
+                time.sleep(int(config.get("Chat", "edit_delay")))
+
+        # Start the defragmenter and ignore the initial KeyError exception
+        thread = threading.Thread(target=fragmenter)
+        thread.start()
+
+        # Tokenize the response into fragmented responses
+        for token in llm.generate(tokens):
+            fragresp = llm.detokenize(token)
+            response = response + fragresp
+
+            # Send initial message
+            if not sent_message:
+                msg = context.bot.send_message(chat_id=update.effective_chat.id, text=response,
+                                            reply_to_message_id=update.message.message_id)
+                context.user_data['bot_last_message_id'] = msg.message_id
+                sent_message = True
+                time.sleep(int(config.get("Chat", "edit_delay")))
+        prompt_is_done = True
+
+        # Edit the message one last time with the final response
+        try:
+            context.bot.editMessageText(chat_id=update.message.chat_id,
+                                        message_id=context.user_data['bot_last_message_id'],
+                                        text=response)
+        except BadRequest:
+            pass
 
 # Bot handlers
 ai_handler = CommandHandler('ai', ai)
 
-#Bot dispatchers
+# Bot dispatchers
 dispatcher.add_handler(ai_handler)
 
 # Bot polling
