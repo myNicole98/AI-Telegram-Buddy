8dd0ba1a949ab0d5ba8abb07166c4b5ea4a6ef6e
nikko <frega.nicole@gmail.com>
2023-05-29 10:21:42 +0200

    Added flood control handling and "is typing" state

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from ctransformers import AutoModelForCausalLM
-from telegram.error import BadRequest
+from telegram.error import BadRequest, RetryAfter
 from telegram.ext import *
 import multiprocessing
 import configparser
 import threading
+import telegram
 import time
 
 # Config initialization
@@ -31,18 +32,31 @@ llm = AutoModelForCausalLM.from_pretrained(path, model_type=model_type, lib=conf
 max_parallel_processes = int(config.get("Chat", "max_parallel_chats"))
 process_semaphore = multiprocessing.Semaphore(max_parallel_processes)
 
-def ai(update, context):
-    process = multiprocessing.Process(target=inference, args=(update, context))
-    process.start()
+# Define process manager
+manager = multiprocessing.Manager()
+active_processes_count = manager.Value('i', 0)
+active_processes_lock = manager.Lock()
 
+def ai(update, context):
+    # Increase the value of the processes count
+    with active_processes_lock:
+        active_processes_count.value += 1
+
+    # Start a new chat
+    process = multiprocessing.Process(target=inference, args=(update, context, active_processes_count, active_processes_lock))
+    process.start()
+    
 # AI command
-def inference(update, context):
+def inference(update, context, active_processes_count, active_processes_lock):
+    # Execute with the limit imposed by max_parallel_processes
     with process_semaphore:
+        # Get new delay in seconds
+        with active_processes_lock:
+            num_active_processes = active_processes_count.value
         # Get message from user
         mex = update.message.text
         mex = mex.replace("/ai", "")
         response = ""
-
         # Define the tokens
         tokens = llm.tokenize(str(config.get("Ai", "prompt") + '\n\n### Instruction:\n\n' + mex + '\n\n ###Response:\n\n'))
 
@@ -61,11 +75,30 @@ def inference(update, context):
                     pass
                 except KeyError:
                     pass
-                time.sleep(int(config.get("Chat", "edit_delay")))
+                except RetryAfter:
+                    print("Flood control triggered. Retrying in 10 seconds...")
+                    time.sleep(10)
+                # Get new delay in seconds
+                with active_processes_lock:
+                    num_active_processes = active_processes_count.value
+                    print(num_active_processes)
+
+                time.sleep(int(config.get("Chat", "edit_delay")) * num_active_processes)
+                print(int(config.get("Chat", "edit_delay")) * num_active_processes)
+
+        def is_typing():
+            # Set bot as typing
+            while not prompt_is_done:
+                context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
+                time.sleep(4)
 
         # Start the defragmenter and ignore the initial KeyError exception
-        thread = threading.Thread(target=fragmenter)
-        thread.start()
+        defragmenter_thread = threading.Thread(target=fragmenter)
+        defragmenter_thread.start()
+
+        # Start the typing state
+        is_typing_thread = threading.Thread(target=is_typing)
+        is_typing_thread.start()
 
         # Tokenize the response into fragmented responses
         for token in llm.generate(tokens):
@@ -78,16 +111,22 @@ def inference(update, context):
                                             reply_to_message_id=update.message.message_id)
                 context.user_data['bot_last_message_id'] = msg.message_id
                 sent_message = True
-                time.sleep(int(config.get("Chat", "edit_delay")))
+                context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
+                time.sleep(int(config.get("Chat", "edit_delay")) * num_active_processes)
         prompt_is_done = True
 
         # Edit the message one last time with the final response
-        try:
-            context.bot.editMessageText(chat_id=update.message.chat_id,
-                                        message_id=context.user_data['bot_last_message_id'],
-                                        text=response)
-        except BadRequest:
-            pass
+        while True:
+            try:
+                context.bot.editMessageText(chat_id=update.message.chat_id,
+                                            message_id=context.user_data['bot_last_message_id'],
+                                            text=response)
+                break
+            except BadRequest:
+                pass
+            except RetryAfter:
+                        print("Flood control triggered. Retrying in 10 seconds...")
+                        time.sleep(10)
 
 # Bot handlers
 ai_handler = CommandHandler('ai', ai)
