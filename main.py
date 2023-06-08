06d0b099939173f447b0ea60210b1f05a6bd4970
nikko <frega.nicole@gmail.com>
2023-06-08 20:04:38 +0200

    New feature: Summary of text documents T0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram.error import BadRequest, RetryAfter, TimedOut, NetworkError
 from ctransformers import AutoModelForCausalLM
+from langchain.llms import CTransformers
 from telegram.ext import *
 import multiprocessing
 import configparser
@@ -7,6 +8,7 @@ import threading
 import telegram
 import sqlite3
 import time
+import os
 
 # Config initialization
 config = configparser.ConfigParser()
@@ -28,6 +30,7 @@ model_size = config.get("Ai", "model_size")
 path = "models/" + model + "-" + model_size + censor + ".bin"
 
 llm = AutoModelForCausalLM.from_pretrained(path, model_type=model_type, lib=config.get("Ai", "instruction_set"), last_n_tokens=256)
+llm_lang = CTransformers(model=path, model_type=model_type, lib=config.get("Ai", "instruction_set"))
 
 # Limit the number of parallel processes
 if config.get("Chat", "history") == "yes":
@@ -50,23 +53,116 @@ def ai(update, context):
     # Increase the value of the processes count
     with active_processes_lock:
         active_processes_count.value += 1
+    
+    # Get message from user
+    mex = update.message.text
+    mex = mex.replace("/ai ", "")
+
+    # Define the tokens
+    tokens = llm.tokenize(str(config.get("Ai", "prompt") + '\n\n### Instruction:\n\n' + mex + '\n\n### Response:\n\n'))
+
+    # Is this using langchain
+    langchain = False
 
     # Start a new chat
-    process = multiprocessing.Process(target=inference, args=(update, context, active_processes_count, active_processes_lock))
+    process = multiprocessing.Process(target=inference, args=(update, context, active_processes_count, active_processes_lock, mex, tokens, langchain))
     process.start()
+
+def summarize(update, context):
+    # Increase the value of the processes count
+    with active_processes_lock:
+        active_processes_count.value += 1
+    with process_semaphore:
+        # Get new delay in seconds
+        with active_processes_lock:
+            num_active_processes = active_processes_count.value
     
+    # Check if directory exists
+    if not os.path.exists('media/summarize/'):
+            os.makedirs('media/summarize/')
+    
+    # Valid file extensions
+    extensions = [".txt"]
+
+    prompt_is_done = False
+    def is_thinking():
+        suspance = 1
+        # Set bot as typing
+        while not prompt_is_done:
+            while True:
+                try:
+                    context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
+                    context.bot.editMessageText(chat_id=update.message.chat_id,
+                                            message_id=context.user_data['bot_last_message_id'],
+                                            text="Summarizing" + "." * suspance)
+                    if suspance >= 3:
+                        suspance = 1
+                    else: 
+                        suspance += 1
+                    time.sleep(4)
+                    break
+                except NetworkError:
+                    time.sleep(1)
+
+    # Get file from user
+    try:
+        file = update.message.reply_to_message.document.get_file()
+        file_path = file['file_path']
+        file_extension = os.path.splitext(file_path)[1]
+        if file_extension in extensions:
+            file.download("media/summarize/document.txt")
+            f = open("media/summarize/document.txt", "r")
+            mex = f.read()
+
+            msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Summarizing",
+                                                reply_to_message_id=update.message.message_id)
+            context.user_data['bot_last_message_id'] = msg.message_id
+
+            # Start the typing state
+            is_thinking_thread = threading.Thread(target=is_thinking)
+            is_thinking_thread.start()
+
+            response = llm_lang(mex)
+            prompt_is_done = True
+
+            while True:
+                try:
+                    context.bot.editMessageText(chat_id=update.message.chat_id,
+                                                message_id=context.user_data['bot_last_message_id'],
+                                                text=response)
+                    break
+                #except BadRequest:
+                #    time.sleep(1)
+                #    pass
+                except RetryAfter:
+                            print("Flood control triggered. Retrying in 10 seconds...")
+                            time.sleep(10)
+                except TimedOut:
+                    pass
+                except NetworkError:
+                    pass
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                        text="Invalid text file")
+            return None
+
+    except AttributeError:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                        text="No file selected")
+    # Decrease the process count
+    with active_processes_lock:
+        active_processes_count.value -= 1
+
 # AI command
-def inference(update, context, active_processes_count, active_processes_lock):
+def inference(update, context, active_processes_count, active_processes_lock, mex, tokens, langchain):
     # Execute with the limit imposed by max_parallel_processes
     with process_semaphore:
         # Get new delay in seconds
         with active_processes_lock:
             num_active_processes = active_processes_count.value
-        # Get message from user
-        mex = update.message.text
-        mex = mex.replace("/ai ", "")
-        response = ""
         
+        # Initialize response
+        response = ""
         if config.get("Chat", "history") == "yes":
             # Initialize database
             history_db = sqlite3.connect("history.db")
@@ -89,10 +185,7 @@ def inference(update, context, active_processes_count, active_processes_lock):
                     history_db.close()
                     break
                 except sqlite3.OperationalError:
-                    pass
-        # Define the tokens
-        tokens = llm.tokenize(str(config.get("Ai", "prompt") + '\n\n### Instruction:\n\n' + mex + '\n\n### Response:\n\n'))
-        
+                    pass        
 
         # Initialize variables
         sent_message = False
@@ -163,7 +256,7 @@ def inference(update, context, active_processes_count, active_processes_lock):
                             time.sleep(1)
                     time.sleep(int(config.get("Chat", "edit_delay")) * num_active_processes)
                 except BadRequest: pass
-        
+
         # Edit the message one last time with the final response
         while True:
             try:
@@ -226,13 +319,16 @@ def clear(update, context):
     else:
         msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Chat history is not enabled. Nothing to clear",
                                         reply_to_message_id=update.message.message_id)
+
 # Bot handlers
 ai_handler = CommandHandler('ai', ai)
 clear_handler = CommandHandler('clear', clear)
+summarize_handler = CommandHandler('summarize', summarize)
 
 # Bot dispatchers
 dispatcher.add_handler(ai_handler)
 dispatcher.add_handler(clear_handler)
+dispatcher.add_handler(summarize_handler)
 
 # Bot polling
 updater.start_polling()
